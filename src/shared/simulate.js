d307de678d968711ffc98084bfd9328e96ef65c4
ZeroTix <weddnremy@gmail.com>
2025-05-17 22:53:14 -0400

    new physics

--- src/shared/simulate.js
+++ src/shared/simulate.js
@@ -31,8 +31,8 @@ function intersectRectCircle(rect, circle) {
 const knock = 100;
 // const accel = 1100;
 // const friction = 0.9;
-const accel = 1200;
-const friction = 0.95;
+const accel = 3500;
+const friction = 0.75;
 function simulatePlayer(player, state, Input, delta) {
    const input = Input === undefined ? player.input : Input;
    player.input = { up: input.up, left: input.left, down: input.down, right: input.right };
